ccb505b63c0a18043232ac41c07d7e3513258e00
Simos Xenitellis <simos.bugzilla@gmail.com>
2009-06-05 18:10:16 -0700

    Added Google Friend Connect settings (oh, u change ID to yours)

--- header.php
+++ header.php
@@ -16,10 +16,40 @@
 <?php // wp_enqueue_script('jquery'); ?>
 <?php wp_head(); ?>
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
+<script type="text/javascript" src="http://www.google.com/friendconnect/script/friendconnect.js"></script>
 <?php comment_tabs(); ?>
 </head>
 
 <body>
+
+<!-- Define the div tag where the gadget will be inserted. -->
+<div id="div-428768660612026067"></div>
+<!-- Render the gadget into a div. -->
+<script type="text/javascript">
+var skin = {};
+skin['BORDER_COLOR'] = '#cccccc';
+skin['ENDCAP_BG_COLOR'] = '#e0ecff';
+skin['ENDCAP_TEXT_COLOR'] = '#333333';
+skin['ENDCAP_LINK_COLOR'] = '#0000cc';
+skin['ALTERNATE_BG_COLOR'] = '#ffffff';
+skin['CONTENT_BG_COLOR'] = '#ffffff';
+skin['CONTENT_LINK_COLOR'] = '#0000cc';
+skin['CONTENT_TEXT_COLOR'] = '#333333';
+skin['CONTENT_SECONDARY_LINK_COLOR'] = '#7777cc';
+skin['CONTENT_SECONDARY_TEXT_COLOR'] = '#666666';
+skin['CONTENT_HEADLINE_COLOR'] = '#333333';
+skin['POSITION'] = 'top';
+skin['DEFAULT_COMMENT_TEXT'] = '- add your comment here -';
+skin['HEADER_TEXT'] = 'Comments';
+google.friendconnect.container.setParentUrl('/blog/' /* location of rpc_relay.html and canvas.html */);
+google.friendconnect.container.renderSocialBar(
+ { id: 'div-428768660612026067',
+   site: '06288059086907762112',
+   'view-params':{"scope":"SITE","features":"video,comment","showWall":"true"}
+ },
+  skin);
+</script>
+
 <div id="body">
 <div id="header">
 <div id="top"><h1 id="logo"><a name="top" title="<?php bloginfo('name'); ?>" href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a> <small><?php bloginfo('description'); ?></small></h1></div>
